eaac70837b9158aa216ca22ab2650b95006b7a0f
cieslikprzemyslaw <cprzemek91@hotmail.com>
2020-09-02 22:36:38 +0100

    Home page finished code

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Menu.scss';
+
+const Menu = () => {
+    return (
+        <nav className="menu">
+            <ul className="menu__list">
+                <li className="menu__item"><Link className="menu__link" to="/" >Home</Link></li>
+                <li className="menu__item"><Link className="menu__link" to="/skills" >Skills</Link></li>
+                <li className="menu__item"><Link className="menu__link" to="/projects" >Projects</Link></li>
+                <li className="menu__item"><Link className="menu__link" to="/about" >About</Link></li>
+                <li className="menu__item"><Link className="menu__link" to="/contact" >Contact</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Menu;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import './Home.scss';
+
+import Menu from '../../components/menu/Menu';
+
+const titles = ['Junior Javascript Developer', 'Junior Front End Developer', 'Junior React Developer']
+
+const Home = () => {
+    const [titlesIndex, setTitlesIndex] = useState(0);
+
+    const changeTitles = () => {
+        setTitlesIndex(titlesIndex => titlesIndex + 1);
+        if (titlesIndex === titles.length - 1) return setTitlesIndex(0);
+    }
+
+    useEffect(() => {
+        const intervalIndex = setInterval(changeTitles, 1000)
+        return function clear() {
+            clearInterval(intervalIndex)
+        }
+    })
+
+    return (
+        <div className="home">
+            <section className="home__overlay">
+                <Menu />
+                <section className="home__title" >
+                    <h1 className="home__welcome" >Welcome</h1>
+                    <h2 className="home__subtitle"><span className="home__description">My name is Przemyslaw Cieslik</span><span className="home__description home__description--change"> I am {titles[titlesIndex]}</span> </h2>
+                </section>
+            </section>
+        </div>
+    );
+};
+
+export default Home;
