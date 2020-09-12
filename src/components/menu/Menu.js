7f00e5c32c160da1c38b87b412e4b8c5a9d90171
cieslikprzemyslaw <cprzemek91@hotmail.com>
2020-09-12 22:15:31 +0100

    Added menu background

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './_Menu.scss';
 
 const Menu = () => {
+    const [isActiveNavbar, setIsActiveNavbar] = useState(false);
+
+    const changeBackground = () => {
+        if(window.scrollY >= 220)(setIsActiveNavbar(true))
+        if(window.scrollY <220)(setIsActiveNavbar(false))
+    }
+
+    window.addEventListener("scroll", changeBackground);
+
     return (
-        <nav className="menu">
+        <nav className={isActiveNavbar ? "menu--active":"menu"}>
             <ul className="menu__list">
                 <li className="menu__item"><NavLink className="menu__link" exact to="/" >Home</NavLink></li>
                 <li className="menu__item"><NavLink className="menu__link" to="/skills" >Skills</NavLink></li>
